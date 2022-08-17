c36ffae951fbc9d9d0736e4056899206198235af
bbosmedia <bbosmedia@users.noreply.github.com>
2022-08-17 13:16:18 +0500

    Finished

--- src/middleware/CheckAuth.js
+++ src/middleware/CheckAuth.js
@@ -2,12 +2,12 @@ import ApiError from '../exceptions/ApiError.js'
 import TokenService from '../services/TokenService.js'
 
 const CheckAuth = async (req, res, next) => {
-	console.log(req.headers)
 	try {
 		const user = await TokenService.verifyToken(req.headers.authorization)
 		if (!user) {
 			throw ApiError.UnauthorizedError()
 		}
+		req.userId = user.id
 		next()
 	} catch (e) {
 		next(e)
